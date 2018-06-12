73b977b2ffd0acdd74437ac140c9ec77adf3db5e
Wojciech Grabowski <wojtek@wojciech-grabowski.pl>
2018-06-12 13:14:24 +0200

    Initial version

---
+++ ng-all.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+const {exec} = require('child_process');
+const path = require('path');
+const {cwd} = require('process');
+
+const angularFile = require(path.join(cwd(), './angular.json'));
+const projects = Object.keys(angularFile.projects);
+
+const [, , ngCommand, ...ngCommandArgs] = process.argv;
+if(ngCommand) {
+  projects.forEach(project => {
+    exec(`ng ${ngCommand} ${project} ${ngCommandArgs.join(' ')}`, (err, stdout) => {
+      if(err) {
+        console.error(err);
+        return;
+      }
+      console.log("%s",stdout);
+    });
+  });
+}
